4ee2d5fb14adae3469292e9dde3087479792fa22
Mahdiou Diallo <104755555+mahdiou-diallo@users.noreply.github.com>
2023-08-04 14:10:28 +0200

    [solution] 2015 - 02 - rust (#2)
    Day 3: Perfectly Spherical Houses in a Vacuum
    
    https://adventofcode.com/2015/day/3

--- 2015/rust/src/main.rs
+++ 2015/rust/src/main.rs
@@ -3,4 +3,5 @@ use rust::solutions::*;
 fn main() {
     println!("day 1 - {:?}", day01::solve());
     println!("day 2 - {:?}", day02::solve());
+    println!("day 3 - {:?}", day03::solve());
 }
---
+++ 2015/rust/src/solutions/day03.rs
@@ -0,0 +1,104 @@
+use std::collections::HashMap;
+use std::fs;
+
+pub fn solve() -> (i32, i32) {
+    let text = fs::read_to_string("../data/03.txt").unwrap();
+    (part1(&text), part2(&text))
+}
+
+#[derive(Hash, PartialEq, Eq, Debug, Clone, Copy)]
+struct Pos(i32, i32);
+
+fn part1(text: &str) -> i32 {
+    text.lines()
+        .next()
+        .unwrap()
+        .chars()
+        .fold(
+            (HashMap::from([(Pos(0, 0), true)]), Pos(0, 0)),
+            |(mut visited, pos), c| {
+                let (dx, dy) = match c {
+                    '^' => (0, 1),
+                    '>' => (1, 0),
+                    'v' => (0, -1),
+                    '<' => (-1, 0),
+                    _ => unreachable!(),
+                };
+                let p = Pos(pos.0 + dx, pos.1 + dy);
+                visited.entry(p).or_insert(true);
+                (visited, p)
+            },
+        )
+        .0
+        .len() as i32
+}
+
+fn part2(text: &str) -> i32 {
+    text.lines()
+        .next()
+        .unwrap()
+        .chars()
+        .enumerate()
+        .fold(
+            (HashMap::from([(Pos(0, 0), true)]), Pos(0, 0), Pos(0, 0)),
+            |(mut visited, pos1, pos2), (idx, c)| {
+                let (dx, dy) = match c {
+                    '^' => (0, 1),
+                    '>' => (1, 0),
+                    'v' => (0, -1),
+                    '<' => (-1, 0),
+                    _ => unreachable!(),
+                };
+
+                let bot_1_moves = idx % 2 == 0;
+                let pos = if bot_1_moves { pos1 } else { pos2 };
+
+                let p = Pos(pos.0 + dx, pos.1 + dy);
+
+                visited.entry(p).or_insert(true);
+
+                if bot_1_moves {
+                    (visited, p, pos2)
+                } else {
+                    (visited, pos1, p)
+                }
+            },
+        )
+        .0
+        .len() as i32
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_visit_2() {
+        assert_eq!(part1(">"), 2);
+    }
+
+    #[test]
+    fn test_visit_4() {
+        assert_eq!(part1("^>v<"), 4);
+    }
+
+    #[test]
+    fn test_visit_2_multiple() {
+        assert_eq!(part1("^v^v^v^v^v"), 2);
+    }
+
+    #[test]
+    fn test_2_visit_3() {
+        assert_eq!(part2("^v"), 3);
+    }
+
+    #[test]
+    fn test_2_visit_3_multiple() {
+        assert_eq!(part2("^>v<"), 3);
+    }
+
+    #[test]
+    fn test_2_visit_11() {
+        assert_eq!(part2("^v^v^v^v^v"), 11);
+    }
+}
